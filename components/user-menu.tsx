2f029ffae0cc667875fbadeb37ba945608955be5
David Tribugenia <david.estrelloso.tribugenia@gmail.com>
2024-05-15 15:06:28 +0800

    fix: signout btn

--- components/user-menu.tsx
+++ components/user-menu.tsx
@@ -30,18 +30,18 @@ export function UserMenu({ user }: UserMenuProps) {
             <div className="text-xs text-zinc-500">{user.username}</div>
           </DropdownMenuItem>
           <DropdownMenuSeparator />
+          <form
+            action={async () => {
+              'use server'
+              await signOut()
+            }}
+          >
+            <button className=" relative flex w-full cursor-pointer select-none items-center rounded-sm px-2 py-1.5 text-xs outline-none transition-colors hover:bg-red-500 hover:text-white focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50">
+              Sign Out
+            </button>
+          </form>
         </DropdownMenuContent>
       </DropdownMenu>
-      <form
-        action={async () => {
-          'use server'
-          await signOut()
-        }}
-      >
-        <button className=" relative flex w-full cursor-pointer select-none items-center rounded-sm px-2 py-1.5 text-xs outline-none transition-colors hover:bg-red-500 hover:text-white focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50">
-          Sign Out
-        </button>
-      </form>
     </div>
   )
 }
