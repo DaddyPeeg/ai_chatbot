82fbbd14970674791b2e7a3ba3ddfca92635cacc
David Tribugenia <david.estrelloso.tribugenia@gmail.com>
2024-05-29 09:59:19 +0800

    update landing

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -2,24 +2,26 @@ import Banner from '@/components/banner'
 import Hero from './components/Hero'
 import InfiniteLogoScroll from '@/components/infinite-logo-scroll'
 import Testimonials from './components/Testimonials'
+import Page_3 from './components/Page_3'
 
 export default function LandingPage() {
   return (
     <div className="mt-16">
       <Banner variant="main">
         <div className="flex justify-center">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, iste!
+          Start Your Journey to Finding the Ideal Health Insurance Plan Today!
         </div>
       </Banner>
       <Hero />
       <Banner variant="accent">
         <div className="flex justify-center text-center text-lg font-medium">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur,
-          necessitatibus.
+          Explore Our Trusted Network of Industry Partnerships for Comprehensive
+          Health Insurance Coverage Options
         </div>
       </Banner>
       <InfiniteLogoScroll />
       <Testimonials />
+      <Page_3 />
     </div>
   )
 }
--- components/chat-loading.tsx
+++ components/chat-loading.tsx
@@ -3,240 +3,26 @@ import { Skeleton } from '@/components/ui/skeleton'
 import { Separator } from '@/components/ui/separator'
 import Wrapper from './wrapper'
 import { ChatPanel } from './chat-panel'
+import Image from 'next/image'
+import { ChatBotIcon } from '@/public/assets'
 
 const ChatLoading = () => {
   return (
-    <Wrapper>
-      <div className="justify-center h-dvh hidden xl:flex">
-        <div className="flex flex-col gap-8 w-[50rem] relative items-center py-8">
-          <div className="flex space-x-4">
-            <Skeleton className="h-12 w-12 rounded-full" />
-            <div className="space-y-2">
-              <Skeleton className="h-4 w-[500px]" />
-              <Skeleton className="h-4 w-[500px]" />
-              <Skeleton className="h-4 w-[500px]" />
-              <Skeleton className="h-4 w-[450px]" />
-            </div>
-          </div>
-          <Separator />
-          <div className="flex space-x-4">
-            <Skeleton className="h-12 w-12 rounded-full" />
-            <div className="space-y-2">
-              <Skeleton className="h-4 w-[500px]" />
-              <Skeleton className="h-4 w-[500px]" />
-              <Skeleton className="h-4 w-[500px]" />
-              <Skeleton className="h-4 w-[450px]" />
-            </div>
-          </div>
-          <Separator />
-          <div className="flex space-x-4">
-            <Skeleton className="h-12 w-12 rounded-full" />
-            <div className="space-y-2">
-              <Skeleton className="h-4 w-[500px]" />
-              <Skeleton className="h-4 w-[500px]" />
-              <Skeleton className="h-4 w-[500px]" />
-              <Skeleton className="h-4 w-[450px]" />
-            </div>
-          </div>
-          <Separator />
-          <div className="flex space-x-4">
-            <Skeleton className="h-12 w-12 rounded-full" />
-            <div className="space-y-2">
-              <Skeleton className="h-4 w-[500px]" />
-              <Skeleton className="h-4 w-[500px]" />
-              <Skeleton className="h-4 w-[500px]" />
-              <Skeleton className="h-4 w-[450px]" />
-            </div>
-          </div>
-          <Separator />
-          <div className="flex space-x-4">
-            <Skeleton className="h-12 w-12 rounded-full" />
-            <div className="space-y-2">
-              <Skeleton className="h-4 w-[500px]" />
-              <Skeleton className="h-4 w-[500px]" />
-              <Skeleton className="h-4 w-[500px]" />
-              <Skeleton className="h-4 w-[450px]" />
-            </div>
-          </div>
-          <div className="absolute bottom-0 w-full">
-            <div className="h-32 flex flex-col items-center rounded-t-lg border border-b-0">
-              <div className="flex gap-4 w-full  px-4">
-                <Skeleton className="h-12 w-[93%] my-4" />
-                <Skeleton className="h-12 w-12 my-4" />
-              </div>
-              <Skeleton className="h-4 w-[50%]" />
-            </div>
-          </div>
-        </div>
+    <div className="w-full h-dvh flex justify-center items-center">
+      <div className="flex flex-col items-center">
+        <h1 className="font-avenirnextltproregular text-3xl">
+          Healthcare Chat Bot
+        </h1>
+        <h2 className="">I'm here for your insurance needs</h2>
+        <Image
+          src={ChatBotIcon}
+          alt="chat-bot-icon"
+          width={150}
+          height={150}
+          className="animate-pulse mt-4"
+        />
       </div>
-      <div className="hidden justify-center h-dvh lg:flex">
-        <div className="flex flex-col gap-8 relative items-center py-8">
-          <div className="flex space-x-4">
-            <Skeleton className="h-10 w-10 rounded-full" />
-            <div className="space-y-2">
-              <Skeleton className="h-2 w-[400px]" />
-              <Skeleton className="h-2 w-[400px]" />
-              <Skeleton className="h-2 w-[400px]" />
-              <Skeleton className="h-2 w-[350px]" />
-            </div>
-          </div>
-          <Separator />
-          <div className="flex space-x-4">
-            <Skeleton className="h-10 w-10 rounded-full" />
-            <div className="space-y-2">
-              <Skeleton className="h-2 w-[400px]" />
-              <Skeleton className="h-2 w-[400px]" />
-              <Skeleton className="h-2 w-[400px]" />
-              <Skeleton className="h-2 w-[350px]" />
-            </div>
-          </div>
-          <Separator />
-          <div className="flex space-x-4">
-            <Skeleton className="h-10 w-10 rounded-full" />
-            <div className="space-y-2">
-              <Skeleton className="h-2 w-[400px]" />
-              <Skeleton className="h-2 w-[400px]" />
-              <Skeleton className="h-2 w-[400px]" />
-              <Skeleton className="h-2 w-[350px]" />
-            </div>
-          </div>
-          <Separator />
-          <div className="flex space-x-4">
-            <Skeleton className="h-10 w-10 rounded-full" />
-            <div className="space-y-2">
-              <Skeleton className="h-2 w-[400px]" />
-              <Skeleton className="h-2 w-[400px]" />
-              <Skeleton className="h-2 w-[400px]" />
-              <Skeleton className="h-2 w-[350px]" />
-            </div>
-          </div>
-          <Separator />
-          <div className="flex space-x-4">
-            <Skeleton className="h-10 w-10 rounded-full" />
-            <div className="space-y-2">
-              <Skeleton className="h-2 w-[400px]" />
-              <Skeleton className="h-2 w-[400px]" />
-              <Skeleton className="h-2 w-[400px]" />
-              <Skeleton className="h-2 w-[350px]" />
-            </div>
-          </div>
-          <Separator />
-          <div className="absolute bottom-0 w-full">
-            <div className="h-24 flex flex-col items-center rounded-t-lg border border-b-0">
-              <div className="flex gap-4 w-full px-4">
-                <Skeleton className="h-8 w-[93%] my-4" />
-                <Skeleton className="h-8 w-8 my-4" />
-              </div>
-              <Skeleton className="h-3 w-[50%]" />
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="justify-center h-dvh flex lg:hidden">
-        <div className="flex flex-col gap-4 relative items-center py-8">
-          <div className="flex space-x-2">
-            <Skeleton className="h-5 w-5 rounded-full" />
-            <div className="space-y-1">
-              <Skeleton className="h-1 w-[200px]" />
-              <Skeleton className="h-1 w-[200px]" />
-              <Skeleton className="h-1 w-[200px]" />
-              <Skeleton className="h-1 w-[150px]" />
-            </div>
-          </div>
-          <Separator />
-          <div className="flex space-x-2">
-            <Skeleton className="h-5 w-5 rounded-full" />
-            <div className="space-y-1">
-              <Skeleton className="h-1 w-[200px]" />
-              <Skeleton className="h-1 w-[200px]" />
-              <Skeleton className="h-1 w-[200px]" />
-              <Skeleton className="h-1 w-[150px]" />
-            </div>
-          </div>
-          <Separator />
-          <div className="flex space-x-2">
-            <Skeleton className="h-5 w-5 rounded-full" />
-            <div className="space-y-1">
-              <Skeleton className="h-1 w-[200px]" />
-              <Skeleton className="h-1 w-[200px]" />
-              <Skeleton className="h-1 w-[200px]" />
-              <Skeleton className="h-1 w-[150px]" />
-            </div>
-          </div>
-          <Separator />
-          <div className="flex space-x-2">
-            <Skeleton className="h-5 w-5 rounded-full" />
-            <div className="space-y-1">
-              <Skeleton className="h-1 w-[200px]" />
-              <Skeleton className="h-1 w-[200px]" />
-              <Skeleton className="h-1 w-[200px]" />
-              <Skeleton className="h-1 w-[150px]" />
-            </div>
-          </div>
-          <Separator />
-          <div className="flex space-x-2">
-            <Skeleton className="h-5 w-5 rounded-full" />
-            <div className="space-y-1">
-              <Skeleton className="h-1 w-[200px]" />
-              <Skeleton className="h-1 w-[200px]" />
-              <Skeleton className="h-1 w-[200px]" />
-              <Skeleton className="h-1 w-[150px]" />
-            </div>
-          </div>
-          <Separator />
-          <div className="flex space-x-2">
-            <Skeleton className="h-5 w-5 rounded-full" />
-            <div className="space-y-1">
-              <Skeleton className="h-1 w-[200px]" />
-              <Skeleton className="h-1 w-[200px]" />
-              <Skeleton className="h-1 w-[200px]" />
-              <Skeleton className="h-1 w-[150px]" />
-            </div>
-          </div>
-          <Separator />
-          <div className="flex space-x-2">
-            <Skeleton className="h-5 w-5 rounded-full" />
-            <div className="space-y-1">
-              <Skeleton className="h-1 w-[200px]" />
-              <Skeleton className="h-1 w-[200px]" />
-              <Skeleton className="h-1 w-[200px]" />
-              <Skeleton className="h-1 w-[150px]" />
-            </div>
-          </div>
-          <Separator />
-          <div className="flex space-x-2">
-            <Skeleton className="h-5 w-5 rounded-full" />
-            <div className="space-y-1">
-              <Skeleton className="h-1 w-[200px]" />
-              <Skeleton className="h-1 w-[200px]" />
-              <Skeleton className="h-1 w-[200px]" />
-              <Skeleton className="h-1 w-[150px]" />
-            </div>
-          </div>
-          <Separator />
-          <div className="flex space-x-2">
-            <Skeleton className="h-5 w-5 rounded-full" />
-            <div className="space-y-1">
-              <Skeleton className="h-1 w-[200px]" />
-              <Skeleton className="h-1 w-[200px]" />
-              <Skeleton className="h-1 w-[200px]" />
-              <Skeleton className="h-1 w-[150px]" />
-            </div>
-          </div>
-          <Separator />
-
-          <div className="absolute bottom-0 w-full">
-            <div className="h-12 flex flex-col items-center rounded-t-lg border border-b-0">
-              <div className="flex gap-1 w-full px-1">
-                <Skeleton className="h-4 w-[100%] my-1" />
-                <Skeleton className="h-4 w-4 my-1" />
-              </div>
-              <Skeleton className="h-3 w-[50%] my-2" />
-            </div>
-          </div>
-        </div>
-      </div>
-    </Wrapper>
+    </div>
   )
 }
 
